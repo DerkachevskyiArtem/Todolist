4a164bd64a0f7470ee85303c84ce78cc37d2a5e6
DerkachevskyiArtem <derkachevskyiartem@gmail.com>
2024-07-22 14:29:12 +0300

    Add component Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from './Todolist';
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+   <div className='App'>
+     <Todolist/>
+     <Todolist/>
+     <Todolist/>
+   </div>
   );
 }
+
+
 
 export default App;
