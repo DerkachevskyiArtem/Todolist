29949ef890aad14d42793f0e0acb8aa7d8a7c913
DerkachevskyiArtem <derkachevskyiartem@gmail.com>
2024-07-22 20:59:17 +0300

    Add types Ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
+import {TaskType, Todolist} from './Todolist';
 
 
 function App() {
+
+  let task1 = [
+    { id: 1, title: "CSS", isDone: true},
+    { id: 2, title: "JS", isDone: true},
+    { id: 3, title: "React", isDone: false},
+  ]
+
+  let task2: Array<TaskType> = [
+    { id: 1, title: "Terminator", isDone: true},
+    { id: 2, title: "XXX", isDone: false},
+    { id: 3, title: "Jentlments of fortune", isDone: true},
+  ]
+
   return (
    <div className='App'>
-     <Todolist/>
-     <Todolist/>
-     <Todolist/>
+     <Todolist title="Whtat to lern" tasks={task1}/>
+     <Todolist title="Movies" tasks={task2}/>
    </div>
   );
 }
