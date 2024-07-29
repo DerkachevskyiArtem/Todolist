118bf7d1d18c1046ccbef9008a8a553162402372
DerkachevskyiArtem <derkachevskyiartem@gmail.com>
2024-07-29 08:29:40 +0300

    Add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     { id: 1, title: "CSS", isDone: true},
     { id: 2, title: "JS", isDone: true},
     { id: 3, title: "React", isDone: false},
+    { id: 4, title: "Redux", isDone: false},
   ]
 
   let task2: Array<TaskType> = [
