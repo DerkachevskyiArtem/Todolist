c4ff7aef616e9f95e9b6be2df37500a834fb7669
DerkachevskyiArtem <derkachevskyiartem@gmail.com>
2024-07-29 11:59:24 +0300

    Add buttons with actions types

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,42 @@ import React, { useState } from "react";
 import "./App.css";
 import { TaskType, Todolist } from "./Todolist";
 
+export type FilterValueType = "all" | "completed" | "active";
+
 function App() {
-  let [tasks, setTasks] = useState([
+  let [tasks, setTasks] = useState<Array<TaskType>>([
     { id: 1, title: "CSS", isDone: true },
     { id: 2, title: "JS", isDone: true },
     { id: 3, title: "React", isDone: false },
     { id: 4, title: "Redux", isDone: false },
   ]);
-
-  // let tasks = arr[0];
-  // let setTasks = arr[1];
+  let [filter, setFilter] = useState<FilterValueType>("all");
 
   function removeTask(id: number) {
     let filteredTasks = tasks.filter((t) => t.id !== id);
     setTasks(filteredTasks);
   }
 
+  function changeFilter(value: FilterValueType) {
+    setFilter(value);
+  }
+
+  let tasksForToDoList = tasks;
+  if (filter === "completed") {
+    tasksForToDoList = tasks.filter((t) => t.isDone === true);
+  }
+  if (filter === "active") {
+    tasksForToDoList = tasks.filter((t) => t.isDone === false);
+  }
+
   return (
     <div className="App">
-      <Todolist title="Whtat to lern" tasks={tasks} removeTask={removeTask} />
+      <Todolist
+        title="Whtat to lern"
+        tasks={tasksForToDoList}
+        removeTask={removeTask}
+        changeFilter={changeFilter}
+      />
     </div>
   );
 }
